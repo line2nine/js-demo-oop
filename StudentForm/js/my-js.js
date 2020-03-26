e453f56da54b93438d8596199b99c6ddb7d4df79
line2nine <hailinh203@gmail.com>
2020-03-26 17:46:00 +0700

    Add files

--- StudentForm/js/my-js.js
+++ StudentForm/js/my-js.js
@@ -1,18 +1,17 @@
-const MAX = 20;
+const MAX = 15;
 var form = "";
 
-function Student(orderList, studentID, firstName, lastName, dateBirth, fromCountry, userName, editStudent, delStudent) {
+function Student(orderList, studentID, firstName, lastName, dateBirth, fromCountry, editStudent, delStudent) {
     this.orderList = orderList;
     this.studentID = studentID;
     this.firstName = firstName;
     this.lastName = lastName;
     this.dateBirth = dateBirth;
     this.fromCountry = fromCountry;
-    this.userName = userName;
     this.editStudent = editStudent;
     this.delStudent = delStudent;
 
-    this.showList = function () {
+     this.showList = function (index) {
         form += "<tr class='studentRow'>";
         form += "<td>";
         form += this.orderList;
@@ -21,7 +20,10 @@ function Student(orderList, studentID, firstName, lastName, dateBirth, fromCount
         form += this.studentID;
         form += "</td>";
         form += "<td>";
-        form += this.lastName + " " + this.firstName;
+        form += `<input type="text" id="displayLastName${index}" style="display: none" value="${this.lastName}">` +
+            `<span style="display: block" id="studentLastName${index}"> ${this.lastName}</span>` +
+            `<input type="text" id="displayFirstName${index}" style="display: none" value="${this.firstName}">` +
+            `<span style="display: block" id="studentFirstName${index}"> ${this.firstName}</span>`;
         form += "</td>";
         form += "<td>";
         form += this.dateBirth;
@@ -30,10 +32,8 @@ function Student(orderList, studentID, firstName, lastName, dateBirth, fromCount
         form += this.fromCountry;
         form += "</td>";
         form += "<td>";
-        form += this.userName;
-        form += "</td>";
-        form += "<td>";
-        form += `<input type ='button' value='Sửa' id='edit' ${this.editStudent}>` + " | " + `<button type ='button' id='del' onclick='delStudent(this)'>  ${this.delStudent} </button>`;
+        form += `<input type ='button' value='EDIT' class="editStd" data-id="${index}" id='edit${index}' ${this.editStudent}>` + " | " +
+            `<button type ='button' id='del${index}'>  ${this.delStudent} </button>`;
         form += "</td>";
         form += "</tr>";
 
@@ -52,18 +52,16 @@ function StudentList() {
             let dateBirth = Math.random().toString(8).substring(17) + "/" +
                 Math.random().toString(8).substring(17) + "/" + Math.random().toString(8).substring(16);
             let fromCountry = Math.random().toString(36).substring(5);
-            let userName = Math.random().toString(36).substring(4);
             let editStudent = "";
             let delStudent = "Xóa";
-
-            this.studentList[i] = new Student(orderList, studentID, firstName, lastName, dateBirth, fromCountry, userName, editStudent, delStudent);
+            this.studentList[i] = new Student(orderList, studentID, firstName, lastName, dateBirth, fromCountry, editStudent, delStudent);
         }
     };
 
     this.showStudentList = function () {
         form = "";
         for (let i = 0; i < this.studentList.length; i++) {
-            this.studentList[i].showList();
+            this.studentList[i].showList(i);
         }
     };
 
@@ -75,26 +73,65 @@ function StudentList() {
         let lastName = document.getElementById("addLastName").value;
         let dateBirth = document.getElementById("addDateBirth").value;
         let fromCountry = document.getElementById("addCountry").value;
-        let userName = document.getElementById("addUsername").value;
         let editStudent = "";
         let delStudent = "Xóa";
-        //console.log(this.studentList);
-        this.studentList[count] = new Student(orderList, studentID, firstName, lastName, dateBirth, fromCountry, userName, editStudent, delStudent);
-        this.studentList[count].showList();
+        this.studentList[count] = new Student(orderList, studentID, firstName, lastName, dateBirth, fromCountry, editStudent, delStudent);
+        this.studentList[count].showList(count);
+    };
+
+    this.showEditField = function (index) {
+        let showInputFN = document.getElementById("displayFirstName" + index);
+        let showInputLN = document.getElementById("displayLastName" + index);
+        let listStudentFN = document.getElementById("studentFirstName" + index);
+        let listStudentLN = document.getElementById("studentLastName" + index);
+        if (showInputFN.style.display === 'none' && showInputLN.style.display === 'none') {
+            showInputFN.style.display = 'block';
+            showInputLN.style.display = 'block';
+            listStudentFN.style.display = 'none';
+            listStudentLN.style.display = 'none';
+        }
+    };
+
+    this.saveEditedStudent = function (index) {
+        let newStudentFN = document.getElementById("displayFirstName" + index).value;
+        let newStudentLN = document.getElementById("displayLastName" + index).value;
+        this.studentList[index].firstName = newStudentFN;
+        this.studentList[index].lastName = newStudentLN;
+        this.showStudentList();
+        // this.studentList = [];
+        // for (let i = 0; i < newStudentFN.length && newStudentLN.length; i++) {
+        //     this.studentList.firstName.push(newStudentFN[i].value);
+        //     this.studentList.lastName.push(newStudentLN[i].value);
+        //     newStudentFN[i].style.display = 'none';
+        //     newStudentLN[i].style.display = 'none';
+        // }
+        //this.showStudentList(index);
+        //this.studentList[index].showList(index);
+    };
+
+    this.editButton = function (index) {
+        let buttonName = document.getElementById("edit" + index);
+        if (buttonName.value === "EDIT") {
+            this.showEditField(index);
+            buttonName.value = "SAVE";
+        } else {
+            this.saveEditedStudent(index);
+            buttonName.value = "EDIT";
+        }
     };
 
     this.deleteStudent = function () {
         let count = this.studentList.length;
         let listTableRow = document.getElementsByClassName("studentRow");
         delete listTableRow[count];
-        this.showStudentList();
-        console.log("c");
+        this.saveEditedStudent()
+        //console.log("c");
     };
 }
 
-function delStudent(student) {
-    student.deleteStudent();
-}
+// function editStudent(student) {
+//     student.editButton();
+// }
 
 function showForm() {
     document.getElementById("myForm").style.display = "block";
@@ -104,16 +141,29 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
+let stdList = new StudentList();
+
+// function editStudent(element) {
+//     stdList.editButton(element);
+// }
+
 function main() {
-    let stdList = new StudentList();
     stdList.initStudentList();
     stdList.showStudentList();
     document.getElementById("add").onclick = function () {
         stdList.addStudent();
     };
-    document.getElementById("del").onclick = function () {
-        stdList.deleteStudent();
-    };
+    let anchors = document.getElementsByClassName('editStd');
+    for(let i = 0; i < anchors.length; i++) {
+        let anchor = anchors[i];
+        anchor.onclick = function() {
+            let id = this.getAttribute("data-id");
+            stdList.editButton(id);
+        }
+    }
+    // document.getElementById("del0").onclick = function () {
+    //     stdList.deleteStudent();
+    // };
 }
 
 main();
